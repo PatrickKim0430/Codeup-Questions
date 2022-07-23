f0a3e1c476292c3fbc440d1e0df7317d1687815f
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-07-23 20:32:40 +0900

    Codeup no,1409
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/7/23
    # Problem: Codeup no,1409
    https://codeup.kr/problem.php?id=1409
    # Solution: Using list and int(input()) to solve this problem

---
+++ Codeup (no. 1409).py
@@ -0,0 +1,10 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/7/23
+# Problem: Codeup no,1409
+https://codeup.kr/problem.php?id=1409
+# Solution: Using list and int(input()) to solve this problem
+
+n=list(map(int,input().split()))
+a=int(input())
+
+print(n[a-1])
