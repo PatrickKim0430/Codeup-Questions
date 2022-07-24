2175ff7bee23360438e523e73cae2eadd3122dca
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-07-25 01:10:02 +0900

    Codeup no,1407
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/7/25
    # Problem: Codeup no,1407
    https://codeup.kr/problem.php?id=1407
    # Solution: Using for loop and list to solve this problem

---
+++ Codeup (no. 1407).py
@@ -0,0 +1,11 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/7/25
+# Problem: Codeup no,1407
+https://codeup.kr/problem.php?id=1407
+# Solution: Using for loop and list to solve this problem
+
+
+n=list(input().split())
+
+for i in n:
+    print(i, end="") 
