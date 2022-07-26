cab125d316df56b3cc1c84f7702028e56b8f19bd
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-07-26 09:23:41 +0900

    Codeup no. 1352
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/7/26
    # Problem: Codeup no.1352
    https://codeup.kr/problem.php?id=1352
    # Solution: Using for loop and int(input()) to solve this problem

---
+++ Codeup (no. 1352).py
@@ -0,0 +1,13 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/7/26
+# Problem: Codeup no.1352
+https://codeup.kr/problem.php?id=1352
+# Solution: Using for loop and int(input()) to solve this problem
+
+n=int(input())
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print("*", end="")
+    print(" ")
+    
