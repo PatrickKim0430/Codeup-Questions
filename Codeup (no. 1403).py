f146a7babea7de996159a90a5ad82261db1efd75
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-07-30 09:08:25 +0900

    Codeup no. 1403
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/7/30
    # Problem: Codeup no. 1403
    https://codeup.kr/problem.php?id=1403
    # Solution: Using for loop and list to solve this problem

---
+++ Codeup (no. 1403).py
@@ -0,0 +1,12 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/7/30
+# Problem: Codeup no. 1403
+https://codeup.kr/problem.php?id=1403
+# Solution: Using for loop and list to solve this problem
+
+n=int(input())
+a=list(map(int,input().split()))
+for i in a: 
+    print(i) 
+for i in a:
+    print(i)
