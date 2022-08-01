250d35bad05f043d2f994b034e4011a7ccbe93ae
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-08-01 23:10:22 +0900

    Codeup no. 1251 (Output from 1 to 100)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/8/1
    # Problem: Codeup no,1251, Output from 1 to 100
    https://www.codeup.kr/problem.php?id=1251
    # Solution: Using for loop to solve this problem

---
+++ Codeup (no. 1251).py
@@ -0,0 +1,8 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/8/1
+# Problem: Codeup no,1251, 
+https://www.codeup.kr/problem.php?id=1251
+# Solution: Using for loop to solve this problem
+
+for i in range(1, 101):
+    print(i, end=' ')
