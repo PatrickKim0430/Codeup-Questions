526738de81030ca47fb588379fab6036ffd9b7b3
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-08-07 22:56:47 +0900

    Codeup no,1352 (Output Rectangle 1)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/8/7
    # Problem: Codeup no,1352, Output Rectangle 1
    https://codeup.kr/problem.php?id=1352
    # Solution: Using input and for loop to solve this problem

--- Codeup (no. 1352).py
+++ Codeup (no. 1352).py
@@ -1,8 +1,8 @@
 # Programmer:  Patrick Junhee Kim 
-# Date: 2022/7/26
-# Problem: Codeup no.1352
+# Date: 2022/8/7
+# Problem: Codeup no,1352, Output Rectangle 1
 https://codeup.kr/problem.php?id=1352
-# Solution: Using for loop and int(input()) to solve this problem
+# Solution: Using input and for loop to solve this problem
 
 n=int(input())
 
@@ -10,4 +10,3 @@ for i in range(1,n+1):
     for j in range(1,n+1):
         print("*", end="")
     print(" ")
-    
