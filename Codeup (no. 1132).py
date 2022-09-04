f52b96ebea97ce431ce72ff6ed1c2d32d269f96a
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-09-04 21:38:41 +0900

    Codeup no. 1132 (Output a string)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/9/4
    # Problem: Codeup no. 1132, Output a string
    https://codeup.kr/problem.php?id=1132
    # Solution: Using print and input() to solve this problem

---
+++ Codeup (no. 1132).py
@@ -0,0 +1,8 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/9/4
+# Problem: Codeup no. 1132, Output a string
+https://codeup.kr/problem.php?id=1132
+# Solution: Using print and input() to solve this problem
+
+
+print(input())
