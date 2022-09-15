23f417feb8cd4bac84700a5ade29dc88d2a97fec
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-09-15 21:15:13 +0900

    Codeup no. 6001 (Print)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/9/15
    # Problem: Codeup no. 6001, Print
    https://codeup.kr/problem.php?id=6001
    # Solution: Using print to solve this problem

---
+++ Codeup (no. 6001).py
@@ -0,0 +1,7 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/9/15
+# Problem: Codeup no. 6001, Print
+https://codeup.kr/problem.php?id=6001
+# Solution: Using print to solve this problem
+
+print("Hello")
