b80d508dfac394724ff329654f0a03a45913dca2
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-09-16 13:50:05 +0900

    Codeup no. 6009
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/9/16
    # Problem: Codeup no. 6009 
    https://codeup.kr/problem.php?id=6009
    # Solution: Using input() and print to solve this problem

---
+++ Codeup (no. 6009).py
@@ -0,0 +1,9 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/9/16
+# Problem: Codeup no. 6009 
+https://codeup.kr/problem.php?id=6009
+# Solution: Using input() and print to solve this problem
+
+a = input()
+
+print(a)
