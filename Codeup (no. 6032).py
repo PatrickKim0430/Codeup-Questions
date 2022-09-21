eb7b3649abffe72af6aa0e0dbd9d32a49c12ee59
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-09-21 23:45:19 +0900

    Codeup no. 6032
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/9/21
    # Problem: Codeup no. 6032
    https://codeup.kr/problem.php?id=6032
    # Solution: Using int(input()) to solve this problem

---
+++ Codeup (no. 6032).py
@@ -0,0 +1,8 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/9/21
+# Problem: Codeup no. 6032
+https://codeup.kr/problem.php?id=6032
+# Solution: Using int(input()) to solve this problem
+
+n = int(input())
+print(-n)
