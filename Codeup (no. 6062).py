3c66518bc185c5bf7626421aadb00eb2fb0c350d
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-10-24 23:48:11 +0900

    Codeup no. 6062
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/10/24
    # Problem: Codeup no. 6062
    https://codeup.kr/problem.php?id=6062
    # Solution: Using int(input()) to solve this problem

---
+++ Codeup (no. 6062).py
@@ -0,0 +1,8 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/10/24
+# Problem: Codeup no. 6062
+https://codeup.kr/problem.php?id=6062
+# Solution: Using int(input()) to solve this problem
+
+a.b=input().split()
+print(int(a)^int(b))
