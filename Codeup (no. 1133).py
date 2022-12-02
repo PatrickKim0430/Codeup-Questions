ec5a4daa835febef879392f5fb3e2f18034502de
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-12-02 23:35:30 +0900

    Codeup no. 1133 (String with spaces)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/12/2
    # Problem: Codeup no. 1133, String with spaces
    https://codeup.kr/problem.php?id=1133
    # Solution: Using input() to solve this problem

---
+++ Codeup (no. 1133).py
@@ -0,0 +1,8 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/12/2
+# Problem: Codeup no. 1133, String with spaces
+https://codeup.kr/problem.php?id=1133
+# Solution: Using input() to solve this problem
+
+s= input()
+print(s) 
